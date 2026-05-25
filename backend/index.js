4236d9101f8031976c54c07a068a39301e0e1cd8
Shristi Kumari <shristikumari1910@gmail.com>
2026-05-25 16:36:56 +0530

    Backend folder structure

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route
+app.get("/", (req, res) => {
+    res.send("Shopnest backend working");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Server Start
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
