7ea1f474ad98e07e76273d3d1e495bdeedf25af3
Shristi Kumari <shristikumari1910@gmail.com>
2026-05-26 13:57:37 +0530

    Mongo DB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db")
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -10,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Route
+// Route 
 app.get("/", (req, res) => {
     res.send("Shopnest backend working");
 });
